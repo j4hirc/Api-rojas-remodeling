87332517fa0172e72bb2999cd0dac10079ceab12
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-05-06 21:59:50 -0500

    MaterialsRequestDto mensajes

--- src/main/java/com/rojas/remodeling/Api_rojas_remodeling/dto/request/MaterialsRequestDto.java
+++ src/main/java/com/rojas/remodeling/Api_rojas_remodeling/dto/request/MaterialsRequestDto.java
@@ -1,11 +1,15 @@
 package com.rojas.remodeling.Api_rojas_remodeling.dto.request;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class MaterialsRequestDto {
 
+    @NotBlank(message = "El nombre del material no puede estar vacío")
     private String name;
+    @NotNull(message = "El ID de la categoría es obligatorio")
     private Long categoryId;
 }

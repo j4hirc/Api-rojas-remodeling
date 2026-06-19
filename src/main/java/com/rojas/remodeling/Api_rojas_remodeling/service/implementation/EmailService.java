4d36f504650066cacdc4dc208b40c7c9e6874b8d
j4hirc <112521047+j4hirc@users.noreply.github.com>
2026-06-18 20:01:47 -0500

    ARREGLO DE CORREOS 3.0

--- src/main/java/com/rojas/remodeling/Api_rojas_remodeling/service/implementation/EmailService.java
+++ src/main/java/com/rojas/remodeling/Api_rojas_remodeling/service/implementation/EmailService.java
@@ -1,44 +1,28 @@
-package com.rojas.remodeling.Api_rojas_remodeling.service.implementation; //[cite: 1]
+package com.rojas.remodeling.Api_rojas_remodeling.service.implementation;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class EmailService {
 
-    @Value("${brevo.api.key}")
-    private String apiKey;
+    @Autowired
+    private JavaMailSender mailSender;
 
     public void sendEmail(String toEmail, String subject, String content) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://api.brevo.com/v3/smtp/email";
-
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("api-key", apiKey);
-
-        Map<String, Object> body = new HashMap<>();
-        body.put("sender", Map.of("name", "RemoMN", "email", "juanalberto25423@gmail.com")); // Pon el correo que registraste en Brevo
-        body.put("to", List.of(Map.of("email", toEmail)));
-        body.put("subject", subject);
-        body.put("htmlContent", content);
-
-        HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
-
         try {
-            restTemplate.postForEntity(url, request, String.class);
-            System.out.println("¡Correo enviado con éxito vía Brevo!");
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("juanalberto25423@gmail.com");
+            message.setTo(toEmail);
+            message.setSubject(subject);
+            message.setText(content);
+
+            mailSender.send(message);
+            System.out.println("¡Correo enviado con éxito vía SMTP de Brevo!");
         } catch (Exception e) {
-            System.err.println("Error al enviar el correo: " + e.getMessage());
+            System.err.println("Error al enviar el correo por SMTP: " + e.getMessage());
         }
     }
 }

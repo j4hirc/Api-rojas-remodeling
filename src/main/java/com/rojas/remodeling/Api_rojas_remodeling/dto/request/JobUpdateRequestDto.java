47be7a2e98b91374bb488c4ebbc848c3f37d333f
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-07-23 10:40:20 -0500

    email para los empleados

--- src/main/java/com/rojas/remodeling/Api_rojas_remodeling/dto/request/JobUpdateRequestDto.java
+++ src/main/java/com/rojas/remodeling/Api_rojas_remodeling/dto/request/JobUpdateRequestDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Data
 public class JobUpdateRequestDto {
 
-    @NotBlank(message = "El comentario no puede estar vacio")
+
     private String comment;
     @NotNull(message = "EL id del trabajo no puede ser nulo")
     private Long jobId;
